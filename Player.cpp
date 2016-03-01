f49f262a83428870e63fc43afc46e260cfcd9348
Izamy <kuleshov-n@bk.ru>
2016-03-01 22:58:51 +0300

    Танк может ездить без поворота.

--- Player.cpp
+++ Player.cpp
@@ -4,7 +4,28 @@ Player::Player(): QGraphicsPixmapItem()
 {
     // устанавливаем картинку танка
     setPixmap(QPixmap(":/images/tank.png"));
-
+    setPos(400, 300);
     // уменьшает картинку
     setScale(0.2);
+}
+
+void Player::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Left)
+    {
+        setPos(x() - 10, y());
+    }
+    else if (event->key() == Qt::Key_Right)
+    {
+        setPos(x() + 10, y());
+    }
+    else if (event->key() == Qt::Key_Up)
+    {
+        setPos(x(), y() - 10);
+    }
+    else if (event->key() == Qt::Key_Down)
+    {
+        setPos(x(), y() + 10);
+    }
+
 }
--- Player.h
+++ Player.h
@@ -3,12 +3,14 @@
 
 #include <QObject>
 #include <QGraphicsPixmapItem>
+#include <QKeyEvent>
 
 class Player: public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
     Player();
+    void keyPressEvent(QKeyEvent *event);
 };
 
 #endif // PLAYER_H
